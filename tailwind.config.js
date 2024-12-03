c965175086a7fa81e6c2166f8b96d152a10d9073
wwwrt <tpripon99@gmail.com>
2024-12-03 13:07:11 +0200

    tailwind setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app//*.{vue,js,ts,jsx,tsx,mdx}",
+    "./pages//*.{vue,js,ts,jsx,tsx,mdx}",
+    "./components//*.{vue,js,ts,jsx,tsx,mdx}",
+    "./src//*.{vue,js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
